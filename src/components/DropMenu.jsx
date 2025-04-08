86c1f27fa0793fca7e5b82069a24e94171653e16
Karen <karen.yumi20@gmail.com>
2025-04-08 16:39:51 -0300

    separando o css de alguns casos complexos

--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -35,12 +35,12 @@ export default function DropMenu({ title, options }) {
       onMouseEnter={handleMouseEnter} 
       onMouseLeave={handleMouseLeave} 
     >
-      <Link className='menu-decoration' >{title}</Link>
+      <Link className='menuDecoration' >{title}</Link>
       {isOpen && (
         <ul id="menu-dropdown"  className={isScrolled ? 'scrolled' : ''}>
           {options.map((option, index) => (
             <li key={index} className='menu-dropdown-options'>
-              <Link to={option.path}  className="menu-decoration" onClick={() => setIsOpen(false)}>{option.name}</Link>
+              <Link to={option.path}  className="menuDecoration" onClick={() => setIsOpen(false)}>{option.name}</Link>
             </li>
           ))}
         </ul>
